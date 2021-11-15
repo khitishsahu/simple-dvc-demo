80f58ac9371b4cdf8e2eb5b7de65f81c55a18c23
Khitish Sahu <khitishsahu@Khitishs-MacBook-Pro.local>
2021-11-15 16:22:16 +0530

    web structure added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,15 @@
 import pytest
 
+
 class NotInRange(Exception):
-    def __init__(self,message="Value not in range"):
+    def __init__(self, message="Value not in range"):
         #self.input_ = input_
         self.message = message
         super().__init__(self.message)
 
+
 def test_generic():
     a = 5
     with pytest.raises(NotInRange):
-        if a not in range(10,20):
+        if a not in range(10, 20):
             raise NotInRange
